68295df531e1c191600b045a2a3cdd9c9e59b92f
Pejman Chatrrooz <OMID.Quist@live.com>
2019-01-18 18:26:14 +0330

    Doc fix

--- Olive/Support/Auth/Authenticatable.php
+++ Olive/Support/Auth/Authenticatable.php
@@ -10,10 +10,13 @@ interface Authenticatable {
 
     /**
      * @param string $password
-     * @param int $level 0,1,2
+     * @param int $level
+     * <br>1: password: pure, usage: save into session or cookie
+     * <br>2: password: level 1 already hashed, usage: save into database
+     * <br>3: passowrd: pure, shortcut for <span style="font-family:monospaced">hash(hash(pure,1),2)</span>
      * @return string
      */
-    public static function authPasswordHash($password, $level = 0);
+    public static function authPasswordHash($password, $level = 1);
 
     /** @return string */
     public static function authPasswordField();
