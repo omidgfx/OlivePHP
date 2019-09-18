aa8104fa4e16fb3474ca4fcb3fffe8dc0abf0241
Pejman Chatrrouz <omid.quist@gmail.com>
2019-09-18 20:52:13 +0430

    perf(Olive\Http\Linker): Use Domain Class For Providing Full Address
    Use Domain class instead of manifest for getting current domain

--- Olive/Http/Linker.php
+++ Olive/Http/Linker.php
@@ -6,33 +6,27 @@ abstract class Linker
 {
     private static $src, $srcFull;
 
-    public static function src($source) {
-        if (!self::$src) self::$src = self::push(manifest::ROOT_DIR);
-        return self::$src . trim($source, '/');
+    public static function srcEx($source, $full = false, $full_port = '') {
+        if ($full) return self::srcFull($source, $full_port);
+        return self::src($source);
     }
 
-    public static function srcFull($source, $port = '', $protocol = null) {
+    public static function srcFull($source, $port = '') {
         if (!self::$srcFull) {
             $r = manifest::ROOT_DIR;
 
             if ($r == '/') $r = '';
             if ($r != '') $r = $r . '/';
-            $p             = $protocol == null ? (self::isConnectionSecure() ? 's' : null) : $protocol;
-            self::$srcFull = 'http' . $p . '://' . manifest::DOMAIN . $port . '/' . $r;
+            $p             = Domain::isHttps() ? 's' : null;
+            self::$srcFull = 'http' . $p . '://' . Domain::current() . $port . '/' . $r;
         }
         return self::$srcFull . trim($source, '/');
     }
 
-    public static function srcEx($source, $full = false, $full_port = '', $full_protocol = null) {
-        if ($full) return self::srcFull($source, $full_port, $full_protocol);
-        return self::src($source);
+    public static function src($source) {
+        if (!self::$src) self::$src = self::push(manifest::ROOT_DIR);
+        return self::$src . trim($source, '/');
     }
-
-    private static function isConnectionSecure() {
-        return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-            || $_SERVER['SERVER_PORT'] == 443;
-    }
-
 
     private static function push($str, $char = '/', $left = true, $right = true) {
         if (!$str)
@@ -43,7 +37,6 @@ abstract class Linker
         //push right
         if ($right && $str[strlen($str) - 1] != $char)
             $str .= $char;
-
         //return
         return $str;
     }
