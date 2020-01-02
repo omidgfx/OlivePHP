4b3045ef39de64bf77c8edc096ac48f99cfefd44
Pejman Chatrrouz <omid.quist@gmail.com>
2020-01-02 12:44:14 +0330

    perf(Crypt): add uuid v4

--- Olive/Security/Crypt.php
+++ Olive/Security/Crypt.php
@@ -63,4 +63,11 @@ abstract class Crypt
     public static function decrypt($encryptedInput, $hashseed = manifest::HASH_SEED, $method = 'AES-192-ECB') {
         return openssl_decrypt(base64_decode($encryptedInput), $method, $hashseed);
     }
+
+    public static function uuid(){
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 }
